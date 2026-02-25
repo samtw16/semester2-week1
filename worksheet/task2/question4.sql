5aee5dca43fa719f127d7bc3bd2d47b3e843af61
samtw16 <samwilliamsmobile@gmail.com>
2026-02-25 18:49:04 +0000

    done

--- worksheet/task2/question4.sql
+++ worksheet/task2/question4.sql
@@ -1,3 +1,14 @@
 -- List all students along with the names of the courses they are enrolled in.
 -- Expected Columns:
 -- StudentId, FirstName, LastName, CourseName
+.mode columns
+.headers on
+
+SELECT student.StudentId, student.FirstName, student.LastName, Course.CourseName
+From student
+Join Enrolment 
+ON Enrolment.studentID = student.studentID
+Join Course
+ON course.CourseID = enrolment.CourseID;
+
+
--- worksheet/task2/question5.sql
+++ worksheet/task2/question5.sql
@@ -1,3 +1,16 @@
 -- For each student, calculate the total number of credits from courses they passed. Assume a passing grade is 40 or higher.
 -- Expected Columns:
 -- StudentId, FirstName, LastName, TotalCreditsPassed
+.mode columns
+.headers on
+
+
+SELECT student.StudentId, student.FirstName, student.LastName, SUM(Course.Credits) AS TotalCreditsPassed
+FROM student
+join Enrolment
+on Enrolment.StudentId = student.StudentId
+Join Course
+ON course.CourseID = enrolment.CourseID
+WHERE enrolment.grade >= 40
+Group By student.StudentID, student.FirstName, student.LastName;
+
